1dc878b77c78d31ca86e37abb91a5e1ad35811c2
CASSIO MACIEL LEMOS <cassio@ServerHenrique>
2016-05-12 22:13:41 -0300

    v0.0 - Cabecalho e definicoes basicas do programa

---
+++ Main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##################################################################################################################################################
+#
+##   AnaliseBovespa.sh   ---- Programa destinado a criar graficos de todas as açoes do bovespa rapidamente, permitindo uma rapida análise da bolsa
+#
+#    Autor e manutentor: Cássio Maciel Lemos <cassio.mac.eng@gmail.com>
+#
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+#
+# Esse programa baixa automaticamente os arquivos históricos e diários da bovespa, faz os gráficos e os coloca em um servidor Apache. Dessa forma,
+# pode-se verificar os cerca de 600 ativos do mercado a vista de maneira fácil e rápida.
+#
+# Licença: GPL
+#
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+#
+# Change log / Histórico
+#
+# v0.0 2016-05-12
+#
+#	- Foi feito a primeira versão do cabeçalho.
+#	- Foi criada a conta do GitHub
